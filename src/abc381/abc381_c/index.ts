0d6506ea4bb0d5a894aa901c446fd14633e60d92
HiroM320 <56062913+HiroM320@users.noreply.github.com>
2024-12-08 08:56:22 +0000

    abc381c 解説見て解いた

--- src/abc381/abc381_c/index.ts
+++ src/abc381/abc381_c/index.ts
@@ -1,52 +1,30 @@
+// 解説見て解いた;;
+
 const main = (rawInput: string) => {
   const [N, S] = rawInput.trim().split("\n");
 
   // 真面目に問題通りの条件をやるより / を見つけてその左に 1、右に 2 が並んでいるという条件で探すほうが早そう
 
-  let prevSlashCharAt = 0;
-  let slashCharAt = S.indexOf("/");
+  let slashCharAt = 0;
 
-  // "/" が含まれない文字列は例外
-  if(slashCharAt === -1) {
-    console.log("0");
-    return;
-  }
-
-  // "/" がある時点で 1 文字確定
+  // "/" があることは確定している
   let longest = 1;
 
   while(slashCharAt !== -1) {
-
-    let lastOneCharAt = 0;
-    let lastTwoCharAt = 0;
-
-    for(let oneCharAt= slashCharAt-1; oneCharAt >= 0; oneCharAt--) {
-      if(S.charAt(oneCharAt) !== "1") {
-        lastOneCharAt = oneCharAt+1;
-        break;
-      }
-    }
-
-    for(let twoCharAt = slashCharAt+1; twoCharAt <= S.length-1; twoCharAt++) {
-      if(S.charAt(twoCharAt) !== "2") {
-        lastTwoCharAt = twoCharAt-1;
-        break;
-      }
-    }
-
-    const provisioningLongest = lastTwoCharAt - lastOneCharAt + 1;
-
-    if(provisioningLongest > longest) {
-      // 1文字少なければ偶数じゃないのでOK（やっつけ仕事感）
-      if(provisioningLongest % 2 !== 0) {
-        longest = provisioningLongest;
-      } else {
-        longest = provisioningLongest -1;
-      }
-    }
-
-    prevSlashCharAt = slashCharAt;
     slashCharAt = S.indexOf("/", slashCharAt+1)
+
+    let diff = 0;
+    while(true) {
+      const oneCharAt = slashCharAt - (diff+1);
+      const twoCharAt = slashCharAt + (diff+1);
+      if(oneCharAt < 0 || twoCharAt > S.length-1) break;
+      if(S.charAt(oneCharAt) !== "1" || S.charAt(twoCharAt)!=="2") break;
+
+      diff++;
+    }
+
+    // スラッシュ(1) + 左右の文字列の長さ(diff*2)
+    longest = Math.max(longest, 1 + diff*2)
   }
 
   console.log(`${longest}`);
